3115092585db4008b99fa2fd636a8ebbcc3ef312
Dimoka113 <133571582+Dimoka113@users.noreply.github.com>
2025-11-01 22:47:52 +0400

    Fix dependencies and README

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main(key_exit: str):
             print("{key} нажата. Остановка кликов...".format(key=key_exit))
             sys.exit(0)
 
-        time.sleep(0.1)
+        time.sleep(0.01)
 
 if __name__ == "__main__":
     main(clicks.get_exit_key())
